ded6cb98ec773d557ef1780ed2d1c8a8b373f047
Jordan cotter <jrdn.cttr@gmail.com>
2023-08-24 20:51:04 -0500

    chore: increase version and update elsint rules

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="about">
-    <h1>version 0.9.2</h1>
+    <h1>version {{version}}</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+const packageJson = require('../../package.json')
+
+export default defineComponent({
+  name: `AboutView`,
+  data: () => ({
+    version: packageJson.version,
+  }),
+})
+</script>
