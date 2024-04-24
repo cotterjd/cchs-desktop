9b80b94c8ea2150c85459c30aef5185b86351c5a
Jordan cotter <jrdn.cttr@gmail.com>
2024-04-23 19:12:10 -0500

    feat: add user to filters

--- src/utils/filteredCodes.ts
+++ src/utils/filteredCodes.ts
@@ -1,11 +1,17 @@
 import { noIssueCodes, issueCodes, incompleteCodes } from '@/static'
 import { UnitCode } from '@/types'
 
-export default (filterType: string, unitCodes: UnitCode[]): UnitCode[] => {
-  if (filterType === `no issues`) return filterCodes(unitCodes, noIssueCodes)
-  if (filterType === `issues`) return filterCodes(unitCodes, issueCodes)
-  if (filterType === `incomplete`) return filterCodes(unitCodes, incompleteCodes)
-  return unitCodes
+export default (filterType: string, unitCodes: UnitCode[], user: string): UnitCode[] => {
+  let codes = unitCodes
+  if (filterType === `no issues`) codes = filterCodes(unitCodes, noIssueCodes)
+  if (filterType === `issues`) codes = filterCodes(unitCodes, issueCodes)
+  if (filterType === `incomplete`) codes = filterCodes(unitCodes, incompleteCodes)
+
+  if (user) {
+    return codes.filter(code => code.user === user)
+  }
+
+  return codes
 }
 
 // Won't handle multiple codes
